6bc8e2d70bc584ced5202bbdf9766f0582694617
Benny Boon <bennybwkrap@gmail.com>
2019-07-23 13:29:20 +0800

    Added work procfile

--- bot/bot.js
+++ bot/bot.js
@@ -5,7 +5,6 @@ const TelegrafFlow = require('telegraf-flow');
 const { WizardScene } = TelegrafFlow ;
 
 const bot = new Telegraf("342044341:AAGP75aL8K3VW5YqkObmA6rXNoGSxSESkj0"); //BE5LeadersBot
-// const bot = new Telegraf("349672924:AAF4R7k0AFXYqUhyEyyUHHHKSqdKnuKZIo4"); //Actual Lead1000Bot
 
 //---------BEGIN POLLING--------
 bot.use(memorySession());
--- database/db.js
+++ database/db.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 
 //Lets connect to our database using the DB server URL.
 mongoose.connect("mongodb://heroku_9w1xhcf1:9lol71lcd62ufeurrg9q5e98na@ds143231.mlab.com:43231/heroku_9w1xhcf1");
-// mongoose.connect("mongodb://localhost:27018/leadbotdb");
-// mongodb://localhost:27017/seventhreebot
 
 module.exports = {
     mongoose:mongoose
